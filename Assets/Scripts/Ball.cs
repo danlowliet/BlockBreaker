778ca24f8f171712354b0826817a95afdb683a41
Zoghalchali <mzoghalchali@contractor.ea.com>
2020-11-06 09:56:07 -0800

    add launch ball on start

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    //Config Pram
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    //State
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
     }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 15f);
+        }
+    }
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+
 }
