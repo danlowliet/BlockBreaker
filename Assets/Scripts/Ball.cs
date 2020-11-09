6d248e81087be9dda1fa7d48568936b3a950cb37
Zoghalchali <mzoghalchali@contractor.ea.com>
2020-11-09 10:14:00 -0800

    add audio

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,13 +8,20 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+    
     //State
     Vector2 paddleToBallVector;
     bool hasStarted = false;
+
+    //Cached component refrences
+    AudioSource myAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -41,5 +48,13 @@ public class Ball : MonoBehaviour
         transform.position = paddlePos + paddleToBallVector;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+    }
 
 }
